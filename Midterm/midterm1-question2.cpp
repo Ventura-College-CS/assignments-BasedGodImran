72431b1cc841ba09add4511bebbb379af343ac4c
Imran Ali <aialisimran@gmail.com>
2021-11-29 13:23:50 -0800

    Good start on Q2

--- Midterm/midterm1-question2.cpp
+++ Midterm/midterm1-question2.cpp
@@ -4,4 +4,39 @@
 #include <queue>
  using namespace std;
 
-// Starting Two Now
+class Student
+{
+
+private:
+
+int SID;
+string SName;
+double *scores;
+int numScores;
+
+public :
+Student()
+{
+
+SID = 0;
+SName = "";
+numScores = 0;
+scores = nullptr;
+
+}
+
+Student (int i, string name, double *sc, int n)
+{
+    SID = i;
+    SName = name;
+    scores = sc;
+    numScores = n;
+}
+
+int getID() { return SID; }
+string getName() { return SName; }
+double *getScores() { return scores}
+
+
+
+}
