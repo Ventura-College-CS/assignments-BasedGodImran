d4fe1402c816acf8c782da6cd02a020f414f830a
Imran Ali <aialisimran@gmail.com>
2021-11-29 12:49:48 -0800

    Starting to get into it.. this is ok

---
+++ Midterm/midterm1-question1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#inlcude <vector>
+using namespace std;
+
+class Student
+{
+
+private:
+int SID;
+string SName;
+double *scores;
+
+public:
+
+Student()
+{
+SID=0;
+SName = "";
+scores = nullptr;
+
+}
+
+Student (int i, string name, double *sc)
+{
+    SID = i;
+    SName = name;
+    scores =sc;
+    
+
+}
+
+
+
+
+}
