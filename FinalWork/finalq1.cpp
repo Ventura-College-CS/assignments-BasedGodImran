9e9cd194586581a329db4d0746cedaed36dfd5da
BasedGodImran <89102830+BasedGodImran@users.noreply.github.com>
2021-12-13 11:59:16 -0800

    Second Commit, finally figured out git issues!

--- FinalWork/finalq1.cpp
+++ FinalWork/finalq1.cpp
@@ -1,1 +1,22 @@
-//First Commit after having Major Git Issues
+// Finally getting started!
+
+#include <iostream>
+using namespace std;
+class Course 
+{
+    public:
+        int id,credit;
+        string name;
+        Course(int courseId,string courseName,int courseCredit) 
+        {
+            id=courseId;
+            name=courseName;
+            credit=courseCredit;
+        }
+};
+void swapInt(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
