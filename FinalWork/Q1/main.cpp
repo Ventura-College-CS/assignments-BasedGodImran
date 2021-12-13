f5fdebf4aabe56a3e9fd6f481f63ff8cb40e7808
BasedGodImran <89102830+BasedGodImran@users.noreply.github.com>
2021-12-13 13:04:45 -0800

    Started three, its much easier

--- FinalWork/Q1/main.cpp
+++ FinalWork/Q1/main.cpp
@@ -1,9 +1,9 @@
+// still on this 
+
 #include <iostream>
 #include <fstream>
 #include "Course.h"
-
 using namespace std;
-
 
 
 int binary_search(Course[], int, int);
@@ -16,8 +16,25 @@ int main() {
 
     
     if (data_file.fail()) {
-        
+       
         cout << "Error!!! Failed to open input_data.txt file..." << endl;
         cout << "Make sure input_data.txt file is present in the current working directory." << endl;
         return 1;
+    }
+
+   
+    Course courses[10];
+    int n = 0;
+    while (!data_file.eof()) {
+        int id, credit;
+        string name;
+
+       
+        data_file >> id;
+        data_file >> name;
+        data_file >> credit;
+
+      
+        Course c(id, name, credit);
+        courses[n++] = c;
     }
---
+++ FinalWork/Q3/main.cpp
@@ -0,0 +1,14 @@
+// Question 3
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+// Stack type
+struct Stack
+{
+        int top;
+        unsigned capacity;
+        int* array;
+};
+
