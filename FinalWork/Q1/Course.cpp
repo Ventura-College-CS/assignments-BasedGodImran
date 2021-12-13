495780f9a0407faa180d8b4ac0c5c80e433066a2
BasedGodImran <89102830+BasedGodImran@users.noreply.github.com>
2021-12-13 12:24:40 -0800

    Getting there..

--- FinalWork/Q1/Course.cpp
+++ FinalWork/Q1/Course.cpp
@@ -1,4 +1,4 @@
-// still going
+// still going..
 
 #include "Course.h"
 
@@ -8,20 +8,35 @@ Course::Course() {
     credit = 0;
 }
 
-/**
- *
- * @param id 
- * @param name 
- * @param credit 
- */
 Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit) {}
 
-/**
- * 
- *
- * @return 
- */
+
 int Course::getId() const {
     return id;
 }
 
+
+const string &Course::getName() const {
+    return name;
+}
+
+
+int Course::getCredit() const {
+    return credit;
+}
+
+
+void Course::setId(int id) {
+    this->id = id;
+}
+
+void Course::setName(const string &name) {
+    this->name = name;
+}
+
+void Course::setCredit(int credit) {
+    this->credit = credit;
+}
+void Course::print() const {
+    cout << "Course{id=" << id << ", name=" << name << ", credit=" << credit << "}" << endl;
+}
